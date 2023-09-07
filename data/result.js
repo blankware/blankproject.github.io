bb64b5f42a99f473fdee0fe4a182e43e49661bcf
yazem <agvayazem@gmail.com>
2023-09-07 16:41:07 +0700

    commit lagi

---
+++ data/result.js
@@ -0,0 +1,470 @@
+const dataJson = [
+  {
+    "nama": "Abdi Hidayatullah",
+    "path": "./data/image/Abdi_Hidayatullah-removebg-preview.png"
+  },
+  {
+    "nama": "Adliani Ananda Raiva",
+    "path": "./data/image/Adliani_Ananda_Raiva-removebg-preview.png"
+  },
+  {
+    "nama": "Ahmad Rizkian Zidane",
+    "path": "./data/image/Ahmad_Rizkian_Zidane-removebg-preview.png"
+  },
+  {
+    "nama": "Akhtar Ghaisan Purnawan",
+    "path": "./data/image/Akhtar_Ghaisan_Purnawan-removebg-preview.png"
+  },
+  {
+    "nama": "Aksa Prawira Sukma",
+    "path": "./data/image/Aksa_Prawira_Sukma-removebg-preview.png"
+  },
+  {
+    "nama": "Ananda Hafis Ramadhan",
+    "path": "./data/image/Ananda_Hafis_Ramadhan-removebg-preview.png"
+  },
+  {
+    "nama": "Ananta Prasetyo",
+    "path": "./data/image/Ananta_Prasetyo-removebg-preview.png"
+  },
+  {
+    "nama": "Anita Herman",
+    "path": "./data/image/Anita_Herman-removebg-preview.png"
+  },
+  {
+    "nama": "Aqilah Fauziah M",
+    "path": "./data/image/Aqilah_Fauziah_M-removebg-preview.png"
+  },
+  {
+    "nama": "Aqnia Mazaya",
+    "path": "./data/image/Aqnia_Mazaya-removebg-preview.png"
+  },
+  {
+    "nama": "Arabhella Enggellika",
+    "path": "./data/image/Arabhella_Enggellika-removebg-preview.png"
+  },
+  {
+    "nama": "Arya Faiz Daulla",
+    "path": "./data/image/Arya_Faiz_Daulla-removebg-preview.png"
+  },
+  {
+    "nama": "Aujrina Safira Naila",
+    "path": "./data/image/Aujrina_Safira_Naila-removebg-preview.png"
+  },
+  {
+    "nama": "Aulia Putri Maesa",
+    "path": "./data/image/Aulia_Putri_Maesa-removebg-preview.png"
+  },
+  {
+    "nama": "Aura Auliya Arsyta",
+    "path": "./data/image/Aura_Auliya_Arsyta-removebg-preview.png"
+  },
+  {
+    "nama": "Ayunda Dwirani",
+    "path": "./data/image/Ayunda_Dwirani-removebg-preview.png"
+  },
+  {
+    "nama": "Azka Burujan Kurnialaksono",
+    "path": "./data/image/Azka_Burujan_Kurnialaksono-removebg-preview.png"
+  },
+  {
+    "nama": "Azka Dhiya Alhaq H",
+    "path": "./data/image/Azka_Dhiya_Alhaq_H-removebg-preview.png"
+  },
+  {
+    "nama": "Azyan Lana Fisyauqi",
+    "path": "./data/image/Azyan_Lana_Fisyauqi-removebg-preview.png"
+  },
+  {
+    "nama": "Baiq Fitralia Raisha",
+    "path": "./data/image/Baiq_Fitralia_Raisha-removebg-preview.png"
+  },
+  {
+    "nama": "Cahya Samila",
+    "path": "./data/image/Cahya_Samila-removebg-preview.png"
+  },
+  {
+    "nama": "Diva Triyanti",
+    "path": "./data/image/Diva_Triyanti-removebg-preview.png"
+  },
+  {
+    "nama": "Dona Aulia Rahma",
+    "path": "./data/image/Dona_Aulia_Rahma-removebg-preview.png"
+  },
+  {
+    "nama": "Dzikra Alghufroon",
+    "path": "./data/image/Dzikra_Alghufroon-removebg-preview.png"
+  },
+  {
+    "nama": "Erdi Dzakki Abdullah",
+    "path": "./data/image/Erdi_Dzakki_Abdullah-removebg-preview.png"
+  },
+  {
+    "nama": "Fadiya Istiana",
+    "path": "./data/image/Fadiya_Istiana-removebg-preview.png"
+  },
+  {
+    "nama": "Faiza Azira Naznin",
+    "path": "./data/image/Faiza_Azira_Naznin-removebg-preview.png"
+  },
+  {
+    "nama": "Farhan Arif Rachman",
+    "path": "./data/image/Farhan_Arif_Rachman-removebg-preview.png"
+  },
+  {
+    "nama": "Fathan Ahmad Budiman",
+    "path": "./data/image/Fathan_Ahmad_Budiman-removebg-preview.png"
+  },
+  {
+    "nama": "Fathir",
+    "path": "./data/image/Fathir-removebg-preview.png"
+  },
+  {
+    "nama": "Fatimah Azzahra",
+    "path": "./data/image/Fatimah_Azzahra-removebg-preview.png"
+  },
+  {
+    "nama": "Fatma Fitria",
+    "path": "./data/image/Fatma_Fitria-removebg-preview.png"
+  },
+  {
+    "nama": "Fauzan Arsy Arifa",
+    "path": "./data/image/Fauzan_Arsy_Arifa-removebg-preview.png"
+  },
+  {
+    "nama": "Fauzan Prayoga Ikhwal",
+    "path": "./data/image/Fauzan_Prayoga_Ikhwal-removebg-preview.png"
+  },
+  {
+    "nama": "Fauzil Herman",
+    "path": "./data/image/Fauzil_Herman-removebg-preview.png"
+  },
+  {
+    "nama": "Fawwaz Faris Pratama",
+    "path": "./data/image/Fawwaz_Faris_Pratama-removebg-preview.png"
+  },
+  {
+    "nama": "Fayaza Irfiani",
+    "path": "./data/image/Fayaza_Irfiani-removebg-preview.png"
+  },
+  {
+    "nama": "Galan Jovi Barowski",
+    "path": "./data/image/Galan_Jovi_Barowski-removebg-preview.png"
+  },
+  {
+    "nama": "Habibi Irfando",
+    "path": "./data/image/Habibi_Irfando-removebg-preview.png"
+  },
+  {
+    "nama": "Hadiatho Wafi Adan",
+    "path": "./data/image/Hadiatho_Wafi_Adan-removebg-preview.png"
+  },
+  {
+    "nama": "Hanifah Aulia Putri",
+    "path": "./data/image/Hanifah_Aulia_Putri-removebg-preview.png"
+  },
+  {
+    "nama": "Haura Nazwa Arisva",
+    "path": "./data/image/Haura_Nazwa_Arisva-removebg-preview.png"
+  },
+  {
+    "nama": "Helena Anjela Putri",
+    "path": "./data/image/Helena_Anjela_Putri-removebg-preview.png"
+  },
+  {
+    "nama": "Ifda Laila",
+    "path": "./data/image/Ifda_Laila-removebg-preview.png"
+  },
+  {
+    "nama": "Ikhfa Riskhal Ilmi",
+    "path": "./data/image/Ikhfa_Riskhal_Ilmi-removebg-preview.png"
+  },
+  {
+    "nama": "Indah Aulia Razaq",
+    "path": "./data/image/Indah_Aulia_Razaq-removebg-preview.png"
+  },
+  {
+    "nama": "Indy Dwi Fitriansyah",
+    "path": "./data/image/Indy_Dwi_Fitriansyah-removebg-preview.png"
+  },
+  {
+    "nama": "Joni Anwar Afrizul",
+    "path": "./data/image/Joni_Anwar_Afrizul-removebg-preview.png"
+  },
+  {
+    "nama": "Khailillah Rifkia Putri",
+    "path": "./data/image/Khailillah_Rifkia_Putri-removebg-preview.png"
+  },
+  {
+    "nama": "Khairil Mumtazi",
+    "path": "./data/image/Khairil_Mumtazi-removebg-preview.png"
+  },
+  {
+    "nama": "Lathifah Ferry Shafiya",
+    "path": "./data/image/Lathifah_Ferry_Shafiya-removebg-preview.png"
+  },
+  {
+    "nama": "M. Ilham",
+    "path": "./data/image/M._Ilham-removebg-preview.png"
+  },
+  {
+    "nama": "M. Yazem Agva Roiz",
+    "path": "./data/image/M._Yazem_Agva_Roiz-removebg-preview.png"
+  },
+  {
+    "nama": "Mia Afriyani",
+    "path": "./data/image/Mia_Afriyani-removebg-preview.png"
+  },
+  {
+    "nama": "Muhammad  Hasbi Al Farizi",
+    "path": "./data/image/Muhammad__Hasbi_Al_Farizi-removebg-preview.png"
+  },
+  {
+    "nama": "Muhammad Alfathir",
+    "path": "./data/image/Muhammad_Alfathir-removebg-preview.png"
+  },
+  {
+    "nama": "Muhammad Asyrafly Almuyassar",
+    "path": "./data/image/Muhammad_Asyrafly_Almuyassar-removebg-preview.png"
+  },
+  {
+    "nama": "Muhammad Daffa Alfianov",
+    "path": "./data/image/Muhammad_Daffa_Alfianov-removebg-preview.png"
+  },
+  {
+    "nama": "Muhammad Daffa Aulya",
+    "path": "./data/image/Muhammad_Daffa_Aulya-removebg-preview.png"
+  },
+  {
+    "nama": "Muhammad Dzaki wilindra",
+    "path": "./data/image/Muhammad_Dzaki_wilindra-removebg-preview.png"
+  },
+  {
+    "nama": "Muhammad Fadel",
+    "path": "./data/image/Muhammad_Fadel-removebg-preview.png"
+  },
+  {
+    "nama": "Muhammad Fadhil Pratama",
+    "path": "./data/image/Muhammad_Fadhil_Pratama-removebg-preview.png"
+  },
+  {
+    "nama": "Muhammad Fakhri Ihsan",
+    "path": "./data/image/Muhammad_Fakhri_Ihsan-removebg-preview.png"
+  },
+  {
+    "nama": "Muhammad Hasbi Muzakki",
+    "path": "./data/image/Muhammad_Hasbi_Muzakki-removebg-preview.png"
+  },
+  {
+    "nama": "Muhammad Hifzi Abrar",
+    "path": "./data/image/Muhammad_Hifzi_Abrar-removebg-preview.png"
+  },
+  {
+    "nama": "Muhammad Irfan Asaba",
+    "path": "./data/image/Muhammad_Irfan_Asaba-removebg-preview.png"
+  },
+  {
+    "nama": "Muhammad Nawfal",
+    "path": "./data/image/Muhammad_Nawfal-removebg-preview.png"
+  },
+  {
+    "nama": "Muhammad Pangestu Abdilah",
+    "path": "./data/image/Muhammad_Pangestu_Abdilah-removebg-preview.png"
+  },
+  {
+    "nama": "Muhammad Rafif Rasyid Ziyadh",
+    "path": "./data/image/Muhammad_Rafif_Rasyid_Ziyadh-removebg-preview.png"
+  },
+  {
+    "nama": "Muhammad Satrio",
+    "path": "./data/image/Muhammad_Satrio-removebg-preview.png"
+  },
+  {
+    "nama": "Muhammad Siddiq Parchan",
+    "path": "./data/image/Muhammad_Siddiq_Parchan-removebg-preview.png"
+  },
+  {
+    "nama": "Muhammad Zaaidil Akbar",
+    "path": "./data/image/Muhammad_Zaaidil_Akbar-removebg-preview.png"
+  },
+  {
+    "nama": "Muhammad Zidan Rinov Arkan",
+    "path": "./data/image/Muhammad_Zidan_Rinov_Arkan-removebg-preview.png"
+  },
+  {
+    "nama": "Nadila Sufi Rmadhani",
+    "path": "./data/image/Nadila_Sufi_Rmadhani-removebg-preview.png"
+  },
+  {
+    "nama": "Naifah Rifdah Salsabila",
+    "path": "./data/image/Naifah_Rifdah_Salsabila-removebg-preview.png"
+  },
+  {
+    "nama": "Naila Salsabila",
+    "path": "./data/image/Naila_Salsabila-removebg-preview.png"
+  },
+  {
+    "nama": "Nailah Aprinira",
+    "path": "./data/image/Nailah_Aprinira-removebg-preview.png"
+  },
+  {
+    "nama": "Najla Agil Sahira",
+    "path": "./data/image/Najla_Agil_Sahira-removebg-preview.png"
+  },
+  {
+    "nama": "Najwa Aprilira",
+    "path": "./data/image/Najwa_Aprilira-removebg-preview.png"
+  },
+  {
+    "nama": "Nayla Rifa Syavana",
+    "path": "./data/image/Nayla_Rifa_Syavana-removebg-preview.png"
+  },
+  {
+    "nama": "Neshria Zuliyenti",
+    "path": "./data/image/Neshria_Zuliyenti-removebg-preview.png"
+  },
+  {
+    "nama": "Niswa Mursyida",
+    "path": "./data/image/Niswa_Mursyida-removebg-preview.png"
+  },
+  {
+    "nama": "Nuirhaliza Mumtaza",
+    "path": "./data/image/Nuirhaliza_Mumtaza-removebg-preview.png"
+  },
+  {
+    "nama": "Nur Azizah",
+    "path": "./data/image/Nur_Azizah-removebg-preview.png"
+  },
+  {
+    "nama": "Nur Shodaq Arrusdi",
+    "path": "./data/image/Nur_Shodaq_Arrusdi-removebg-preview.png"
+  },
+  {
+    "nama": "Qanita Khalisa Ramadi",
+    "path": "./data/image/Qanita_Khalisa_Ramadi-removebg-preview.png"
+  },
+  {
+    "nama": "Rafid Gaisan Azra",
+    "path": "./data/image/Rafid_Gaisan_Azra-removebg-preview.png"
+  },
+  {
+    "nama": "Rafid Maulana Zaldas",
+    "path": "./data/image/Rafid_Maulana_Zaldas-removebg-preview.png"
+  },
+  {
+    "nama": "Rafiz Rahmadani",
+    "path": "./data/image/Rafiz_Rahmadani-removebg-preview.png"
+  },
+  {
+    "nama": "Rahman Juliansyah",
+    "path": "./data/image/Rahman_Juliansyah-removebg-preview.png"
+  },
+  {
+    "nama": "Rahmi Aulya Safitri",
+    "path": "./data/image/Rahmi_Aulya_Safitri-removebg-preview.png"
+  },
+  {
+    "nama": "Raisa Nadhira Abrar",
+    "path": "./data/image/Raisa_Nadhira_Abrar-removebg-preview.png"
+  },
+  {
+    "nama": "Rastiana Hafidzah Rasyid",
+    "path": "./data/image/Rastiana_Hafidzah_Rasyid-removebg-preview.png"
+  },
+  {
+    "nama": "Rasyadah Karimah",
+    "path": "./data/image/Rasyadah_Karimah-removebg-preview.png"
+  },
+  {
+    "nama": "Rifa Mulia Putri",
+    "path": "./data/image/Rifa_Mulia_Putri-removebg-preview.png"
+  },
+  {
+    "nama": "Risqa Anada Jivera",
+    "path": "./data/image/Risqa_Anada_Jivera-removebg-preview.png"
+  },
+  {
+    "nama": "Salman Faris Muharram",
+    "path": "./data/image/Salman_Faris_Muharram-removebg-preview.png"
+  },
+  {
+    "nama": "Salsa Billa",
+    "path": "./data/image/Salsa_Billa-removebg-preview.png"
+  },
+  {
+    "nama": "Salwa Maharzil Aufa",
+    "path": "./data/image/Salwa_Maharzil_Aufa-removebg-preview.png"
+  },
+  {
+    "nama": "Sayyid H.Z",
+    "path": "./data/image/Sayyid_H.Z-removebg-preview.png"
+  },
+  {
+    "nama": "Shadiqa Sulthan",
+    "path": "./data/image/Shadiqa_Sulthan-removebg-preview.png"
+  },
+  {
+    "nama": "Shalda Hanifa",
+    "path": "./data/image/Shalda_Hanifa-removebg-preview.png"
+  },
+  {
+    "nama": "Shaqila Sany",
+    "path": "./data/image/Shaqila_Sany-removebg-preview.png"
+  },
+  {
+    "nama": "Shofiyyah Zuhri",
+    "path": "./data/image/Shofiyyah_Zuhri-removebg-preview.png"
+  },
+  {
+    "nama": "Sofi Zihni",
+    "path": "./data/image/Sofi_Zihni-removebg-preview.png"
+  },
+  {
+    "nama": "Subhil Mubarak",
+    "path": "./data/image/Subhil_Mubarak-removebg-preview.png"
+  },
+  {
+    "nama": "Syarif Hidayatullah",
+    "path": "./data/image/Syarif_Hidayatullah-removebg-preview.png"
+  },
+  {
+    "nama": "Syauqi Athallah Abrar",
+    "path": "./data/image/Syauqi_Athallah_Abrar-removebg-preview.png"
+  },
+  {
+    "nama": "Tania Azzahra Rezki",
+    "path": "./data/image/Tania_Azzahra_Rezki-removebg-preview.png"
+  },
+  {
+    "nama": "Taufiqur Rahman",
+    "path": "./data/image/Taufiqur_Rahman-removebg-preview.png"
+  },
+  {
+    "nama": "Tsania Faiza Mardhatillah",
+    "path": "./data/image/Tsania_Faiza_Mardhatillah-removebg-preview.png"
+  },
+  {
+    "nama": "Wardatunnadiyah",
+    "path": "./data/image/Wardatunnadiyah-removebg-preview.png"
+  },
+  {
+    "nama": "Widya Fuadila Putri",
+    "path": "./data/image/Widya_Fuadila_Putri-removebg-preview.png"
+  },
+  {
+    "nama": "Yovalqi Azeni Putra",
+    "path": "./data/image/Yovalqi_Azeni_Putra-removebg-preview.png"
+  },
+  {
+    "nama": "Zafira Najla Rinjani",
+    "path": "./data/image/Zafira_Najla_Rinjani-removebg-preview.png"
+  },
+  {
+    "nama": "Zahra Hannifa",
+    "path": "./data/image/Zahra_Hannifa-removebg-preview.png"
+  },
+  {
+    "nama": "Zahwa Az Zahra",
+    "path": "./data/image/Zahwa_Az_Zahra-removebg-preview.png"
+  }
+]
